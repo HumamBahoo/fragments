3a62cd84e19fd67f2dc6269dcd3017a3752ec918
Humam Bahoo <humam.bahoo@gmail.com>
2023-01-29 16:17:04 -0500

    feat: setup authentication crashing with error message

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,9 @@ module.exports = {
     commonjs: true,
     es2021: true,
     node: true,
+    // let ESLint know we're using Jest.
+    // see: https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+    jest: true,
   },
   extends: 'eslint:recommended',
   overrides: [],
