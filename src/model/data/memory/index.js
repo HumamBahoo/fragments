07d713590be86f11413d37fa1628374aeeeccbc9
Humam Bahoo <humam.bahoo@gmail.com>
2023-03-16 18:45:19 -0400

    Remove unnecessary logger messages

--- src/model/data/memory/index.js
+++ src/model/data/memory/index.js
@@ -1,6 +1,3 @@
-// src/model/data/memory/index.js
-
-const logger = require('../../../logger');
 const MemoryDB = require('./memory-db');
 
 // Create two in-memory databases: one for fragment metadata and the other for raw data
@@ -9,36 +6,26 @@ const metadata = new MemoryDB();
 
 // Write a fragment's metadata to memory db. Returns a Promise
 function writeFragment(fragment) {
-  logger.debug({ fragment }, 'Writing fragment to db');
-
   return metadata.put(fragment.ownerId, fragment.id, fragment);
 }
 
 // Read a fragment's metadata from memory db. Returns a Promise
 function readFragment(ownerId, id) {
-  logger.debug({ ownerId, id }, 'Reading fragment with ownerId/id from db');
-
   return metadata.get(ownerId, id);
 }
 
 // Write a fragment's data buffer to memory db. Returns a Promise
 function writeFragmentData(ownerId, id, buffer) {
-  logger.debug({ ownerId, id }, 'Writing data of a fragment with ownerId/id to db');
-
   return data.put(ownerId, id, buffer);
 }
 
 // Read a fragment's data from memory db. Returns a Promise
 function readFragmentData(ownerId, id) {
-  logger.debug({ ownerId, id }, 'Reading data of a fragment with ownerId/id from db');
-
   return data.get(ownerId, id);
 }
 
 // Get a list of fragment ids/objects for the given user from memory db. Returns a Promise
 async function listFragments(ownerId, expand = false) {
-  logger.debug({ ownerId, expand }, 'Retrieving fragment list of ownerId from db');
-
   const fragments = await metadata.query(ownerId);
 
   // If we don't get anything back, or are supposed to give expanded fragments, return
@@ -52,8 +39,6 @@ async function listFragments(ownerId, expand = false) {
 
 // Delete a fragment's metadata and data from memory db. Returns a Promise
 function deleteFragment(ownerId, id) {
-  logger.debug({ ownerId, id }, 'Deleting fragment with ownerId/id from db');
-
   return Promise.all([
     // Delete metadata
     metadata.del(ownerId, id),
